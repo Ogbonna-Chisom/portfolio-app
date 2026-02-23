8ca2c64247ca52e4b806d22ccb3b108a4024c202
Ogbonna-Chisom <generouschisom14@gmail.com>
2026-02-23 14:13:30 +0100

    Adding all the pages to my hero page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-
+import React from 'react'
 import AppRoutes from './containers/AppRoutes'
-import './assets/css/Navbar.css' 
+import './assets/css/Navbar.css'
 import './assets/css/Footer.css'
 import './assets/css/Hero.css'
 import './assets/css/About.css'
@@ -9,9 +9,13 @@ import './assets/css/Services.css'
 import './assets/css/Contact.css'
 
 
+
+
 const App = () => {
   return (
-    <AppRoutes/>
+    <div>
+      <AppRoutes/>
+    </div>
   )
 }
 
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,16 +1,31 @@
 import { Link } from 'react-router-dom'
 import { routes } from '../config/routes'
+import About from './About'
+import Service from './Service'
+import Project from './Project'
+import Contact from './Contact'
+
 
 const Home = () => {
     return (
-        <div id='home' className='hero'>
-            <h1><span>Hi, I'm Genny Blessed</span></h1>
-            <p>Currently learning front end development in Lagos Nigeria</p>
-            <div className="hero-action">
-                <div className="hero-connect"><Link to={routes.contact} className='anchor-link' offset={50} href='#contact'>Connect with me</Link></div>
-                <div className="hero-resume"><Link to={routes.contact} className='anchor-link' offset={50} href='#contact'>My Resume</Link></div>
+
+        <>
+            <div id='home' className='hero'>
+                <h1><span>Hi, I'm Genny Blessed</span></h1>
+                <p>Currently learning front end development in Lagos Nigeria</p>
+                <div className="hero-action">
+                    <div className="hero-connect"><Link to={routes.contact} className='anchor-link' offset={50} href='#contact'>Connect with me</Link></div>
+                    <div className="hero-resume"><Link to={routes.contact} className='anchor-link' offset={50} href='#contact'>My Resume</Link></div>
+                </div>
             </div>
-        </div>
+            <About/>
+            <Project/>
+            <Service/>
+            <Contact/>
+            
+        </>
+      
+        
     )
 }
 
